66518ae6a669e7876cfb25ba9d15aa950a22614c
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-27 15:57:27 -0600

    First big commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/auth" element={<h1>Home</h1>} />
+          <Route path="/rated" element={<h1>Home</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
