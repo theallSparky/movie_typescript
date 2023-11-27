431fa989e148b1cfd1a706fd294128fcbc0fd8ff
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-27 16:28:17 -0600

    Added links and page redirection functionality

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,17 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
     return <Menu fixed="top" size="huge">
         <Menu.Menu position="right">
-            <Menu.Item as={Link} to="auth"> Auth </Menu.Item>
+            <Menu.Item as={Link} to="/auth" style={{ fontSize: "1.5rem" }}>
+                 Auth 
+            </Menu.Item>
         </Menu.Menu>
+
+        <Menu.Item as={Link} to="/" style={{ fontSize: "1.5rem" }}>
+            Home
+        </Menu.Item>
+    
+        <Menu.Item as={Link} to="/rated" style={{ fontSize: "1.5rem" }}>
+            Rated
+        </Menu.Item>
     </Menu>
 }
