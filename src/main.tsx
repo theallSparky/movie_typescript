36fea695eb189b7b90c8d45656e8d4adbf993f56
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-30 23:24:51 -0600

    Brought in toastify, stylized toaster popups

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
+import { ToastContainer } from 'react-toastify';
+
 
 const queryClient = new QueryClient()
 
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ToastContainer />
     </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/pages/home/column-display.tsx
+++ src/pages/home/column-display.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { rateMovie, rateTvShow } from "./mutation";
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 interface DisplayData {
     id: number;
@@ -24,14 +26,33 @@ export const ColumnDisplay = (props: Props) => {
     const { data, displayType } = props
     const [rating, setRating] = useState<number>(0)
 
+    const onSuccess = () => {
+        toast.success("Successfully rated!", {
+            autoClose: 3000,
+            pauseOnHover: true,
+            draggable: true,
+        })
+    }
+
+    const onError = () => {
+        toast.error("Something went wrong!", {
+            autoClose: 3000,
+            pauseOnHover: true,
+        })
+    }
+
     const { mutate: rateMovieMutation } = useMutation({
         mutationKey: ["rateMovie"],
         mutationFn: (id: number) => rateMovie(id, rating),
+        onSuccess,
+        onError,
     })
 
     const { mutate: rateTvShowMutation } = useMutation({
         mutationKey: ["rateTvShow"],
         mutationFn: (id: number) => rateTvShow(id, rating),
+        onSuccess,
+        onError,
     })
 
 const rate = displayType === DisplayType.Movies ? rateMovieMutation : rateTvShowMutation
